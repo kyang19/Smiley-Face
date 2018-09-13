dda265650b768b2ee1ccc5ee5b4c01e3acb2738d
kyang19 <42977150+kyang19@users.noreply.github.com>
2018-09-13 10:37:22 -0400

    Add files via upload

---
+++ Smile.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @author kimyang
+ *Smiley Face
+ */
+public class Smile 
+{
+
+	public static void main(String[] args) 
+	{System.out.print("\\\\\\\\\\\\\\\\\\\\\\\n|  *    *  |\n|    *     |\n|   \\__/    |"
+			+ "\n\\__________/   ");
+		
+	}
+
+}
